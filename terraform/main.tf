18d915bd3ca11803d39db9598a3613b55e9ea948
Ahmed Hesham Abdelkader <23265119+ahmedhesham6@users.noreply.github.com>
2025-01-09 04:59:28 +0200

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,15 +69,15 @@ resource "aws_security_group" "this" {
   }
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
