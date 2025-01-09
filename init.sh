4614ecfdaea37d251e13410d1cf65496254e6e0e
Ahmed Hesham Abdelkader <23265119+ahmedhesham6@users.noreply.github.com>
2025-01-09 04:12:47 +0200

    fixes

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ set -e
 
 sudo apt update
 
+# Caddy
+sudo apt install caddy
+
 # Docker
 sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -16,14 +19,11 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt update
 
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # USERGROUP
-sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
-
-
-sudo apt install caddy
