efcff16b2ed86ea16b441db8177f28213f1e089b
Jeremy Denquin <jdenquin@gmail.com>
2025-02-11 17:01:19 +0100

    add ga

--- docker/runner.sh
+++ docker/runner.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+declare -A ENV_VARS=(
+    [POSTGRES_PASSWORD]=$(openssl rand -hex 16)
+    [SECRET_KEY_BASE]=$(openssl rand -base64 16)
+    [LAGO_RSA_PRIVATE_KEY]=$(openssl genrsa 2048 | base64 | tr -d '\n')
+    [REDIS_URL]="redis://localhost:6379/0"
+    [LAGO_FRONT_URL]="http://localhost"
+    [LAGO_API_URL]="http://localhost:3000"
+    [API_URL]="http://localhost:3000"
+    [APP_ENV]="production"
+)
+
 if [ -f "/data/.env" ]; then
     for LINE in $(cat /data/.env); do export $LINE; done
 fi
@@ -23,57 +34,39 @@ sed -i "s#DATA_DIR#${DATA_DIR}#g" /etc/redis/redis.conf
 export PGDATA="${DATA_DIR}/postgresql"
 export PGPORT=5432
 
-# Prepare FE Environment
-bash -c ./front/.env.sh
-
 # Start Redis, PG and Nginx
 service redis-server restart >> /dev/null
 service postgresql restart >> /dev/null
+service nginx restart >> /dev/null
 
 # Prepare Environment
-if [ -z "${POSTGRES_PASSWORD}" ]; then
-    export POSTGRES_PASSWORD=$(openssl rand -hex 16)
-    echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" >> ${DATA_DIR}/.env
-fi
-
-if [ -z "${SECRET_KEY_BASE}" ]; then
-    export SECRET_KEY_BASE=$(openssl rand -base64 16)
-    echo "SECRET_KEY_BASE=${SECRET_KEY_BASE}" >> ${DATA_DIR}/.env
-fi
-
-if [ -z "${LAGO_RSA_PRIVATE_KEY}" ]; then
-    export LAGO_RSA_PRIVATE_KEY=$(openssl genrsa 2048 | base64)
-    echo "LAGO_RSA_PRIVATE_KEY=${LAGO_RSA_PRIVATE_KEY}" >> ${DATA_DIR}/.env
-fi
+# Defaulting values
+for VAR in "${!ENV_VARS[@]}"; do
+    if [ -z "${!VAR}" ]; then
+        export $VAR=${ENV_VARS[$VAR]}
+        echo "$VAR=${ENV_VARS[$VAR]}" >> ${DATA_DIR}/.env
+    fi
+done
 
 if [ -z "${DATABASE_URL}" ]; then
     export DATABASE_URL=postgresql://lago:$POSTGRES_PASSWORD@localhost:5432/lago
     echo "DATABASE_URL=${DATABASE_URL}" >> ${DATA_DIR}/.env
 fi
 
-if [ -z "${REDIS_URL}" ]; then
-    export REDIS_URL="redis://localhost:6379/0"
-    echo "REDIS_URL=${REDIS_URL}" >> ${DATA_DIR}/.env
-fi
-
-if [ -z "${LAGO_FRONT_URL}" ]; then
-    export LAGO_FRONT_URL="http://localhost"
-    echo "LAGO_FRONT_URL=${LAGO_FRONT_URL}" >> ${DATA_DIR}/.env
-fi
-
-if [ -z "${LAGO_API_URL}" ]; then
-    export LAGO_API_URL="http://localhost:3000"
-    echo "LAGO_API_URL=${LAGO_API_URL}" >> ${DATA_DIR}/.env
-fi
+# Prepare Front Environment
+cd ./front
+bash -c ./.env.sh
+cd ..
 
 export RAILS_ENV=production
 
 # Create DB User
-su -c "psql -tc \"SELECT 1 FROM pg_user WHERE usename = 'lago';\" | grep -q 1 || psql -c \"CREATE ROLE lago PASSWORD '${POSTGRES_PASSWORD}' CREATEDB LOGIN;\"" postgres
+su -c "psql -tc \"SELECT 1 FROM pg_user WHERE usename = 'lago';\" | grep -q 1 || psql -c \"CREATE ROLE lago PASSWORD '${POSTGRES_PASSWORD}' CREATEDB LOGIN;\"" postgres >> ${DATA_DIR}/db.log
 
 # Launch BE Services
 cd ./api
 bundle exec rake db:create >> ${DATA_DIR}/db.log
 bundle exec rake db:migrate >> ${DATA_DIR}/db.log
 bundle exec rails signup:seed_organization >> ${DATA_DIR}/db.log
+rm -f ./tmp/pids/server.pid
 foreman start
