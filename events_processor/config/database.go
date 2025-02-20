0084e837d8772f47dca092c685683a3ff8100059
Vincent Pochet <vincent@getlago.com>
2025-02-20 17:30:50 +0100

    Add sone basic config

--- events_processor/config/database.go
+++ events_processor/config/database.go
@@ -1,12 +1,34 @@
 package config
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func NewConnection() {
+type DB struct {
+	connection *gorm.DB
+}
+
+func NewConnection() (*DB, error) {
+	dsn := fmt.Sprintf(
+		"user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
+
+	return &DB{connection: db}, nil
 }
 
 func main() {
