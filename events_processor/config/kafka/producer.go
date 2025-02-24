f50bd25f3be7be52a119bafb90bd3c503b015ab7
Vincent Pochet <vincent@getlago.com>
2025-02-24 15:44:39 +0100

    Basic consumer/producer

---
+++ events_processor/config/kafka/producer.go
@@ -0,0 +1,59 @@
+package kafka
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/twmb/franz-go/pkg/kgo"
+)
+
+type ProducerConfig struct {
+	Topic string
+}
+
+type Producer struct {
+	client *kgo.Client
+	config ProducerConfig
+	logger *slog.Logger
+}
+
+type ProducerMessage struct {
+	Key   []byte
+	Value []byte
+}
+
+func NewProducer(cfg *ProducerConfig) (*Producer, error) {
+	opts := make([]kgo.Opt, 0)
+	kcl, err := NewKafkaClient(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	logger := slog.Default()
+	logger = logger.With("component", "kafka-producer")
+
+	pdr := &Producer{
+		client: kcl,
+		config: *cfg,
+		logger: logger,
+	}
+
+	return pdr, nil
+}
+
+func (p *Producer) Produce(ctx context.Context, msg *ProducerMessage) {
+	record := &kgo.Record{
+		Topic: p.config.Topic,
+		Key:   msg.Key,
+		Value: msg.Value,
+	}
+
+	pr := p.client.ProduceSync(ctx, record)
+	if err := pr.FirstErr(); err != nil {
+		p.logger.Error("record had a produce error while synchronously producing", slog.String("error", err.Error()))
+	}
+}
+
+func (p *Producer) Ping(ctx context.Context) error {
+	return p.client.Ping(ctx)
+}
