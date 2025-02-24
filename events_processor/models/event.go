7f1acd1de52fe5eafa11297c0df3946fc536f253
Vincent Pochet <vincent@getlago.com>
2025-02-24 11:23:00 +0100

    Define models and loggers

---
+++ events_processor/models/event.go
@@ -0,0 +1,12 @@
+package models
+
+type Event struct {
+	OrganizationID          string         `json:"organization_id"`
+	ExternalSubscriptionId  string         `json:"external_subscription_id"`
+	TransactionID           string         `json:"transaction_id"`
+	Timestamp               any            `json:"timestamp"`
+	Code                    string         `json:"code"`
+	Properties              map[string]any `json:"properties"`
+	PreciseTotalAmountCents string         `json:"precise_total_amount_cents"`
+	Value                   *string        `json:"value"`
+}
