42bb6105e67804f4eedf3556a0579f6b027e4f68
Vincent Pochet <vincent@getlago.com>
2025-02-28 14:44:51 +0100

    Add logger to gorm

--- events_processor/database/database.go
+++ events_processor/database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"log/slog"
 
+	slogGorm "github.com/orandin/slog-gorm"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -16,7 +17,13 @@ func NewConnection(dbUrl string) (*DB, error) {
 	logger := slog.Default()
 	logger = logger.With("component", "db")
 
-	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
+	gormLogger := slogGorm.New(
+		slogGorm.WithHandler(logger.Handler()),
+	)
+
+	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{
+		Logger: gormLogger,
+	})
 	if err != nil {
 		logger.Error("Error connecting to the database", slog.String("error", err.Error()))
 		return nil, err
