a63f7ef5ca0a7b34e24e5016e10d35d55e57f70a
Vincent Pochet <vincent@getlago.com>
2025-03-04 15:27:34 +0100

    Add support for milliseconds in timestamp

--- events_processor/processor/models/event.go
+++ events_processor/processor/models/event.go
@@ -1,6 +1,13 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"time"
+
+	"github.com/getlago/lago/events-processor/utils"
+)
 
 const HTTP_RUBY string = "http_ruby"
 
@@ -8,7 +15,7 @@ type Event struct {
 	OrganizationID          string         `json:"organization_id"`
 	ExternalSubscriptionID  string         `json:"external_subscription_id"`
 	TransactionID           string         `json:"transaction_id"`
-	Timestamp               int            `json:"timestamp"`
+	Timestamp               any            `json:"timestamp"`
 	Code                    string         `json:"code"`
 	Properties              map[string]any `json:"properties"`
 	PreciseTotalAmountCents string         `json:"precise_total_amount_cents"`
@@ -16,8 +23,37 @@ type Event struct {
 	Source                  string         `json:"source,omotempty"`
 }
 
-func (ev *Event) TimestampAsTime() time.Time {
-	return time.Unix(int64(ev.Timestamp), 0).In(time.UTC)
+func (ev *Event) TimestampAsTime() utils.Result[time.Time] {
+	var seconds int64
+	var nanoseconds int64
+
+	switch timestamp := ev.Timestamp.(type) {
+	case string:
+		floatTimestamp, err := strconv.ParseFloat(timestamp, 64)
+		if err != nil {
+			return utils.FailedResult[time.Time](err)
+		}
+
+		seconds = int64(floatTimestamp)
+		nanoseconds = int64((floatTimestamp - float64(seconds)) * 1e9)
+
+	case int:
+		seconds = int64(timestamp)
+		nanoseconds = 0
+
+	case int64:
+		seconds = timestamp
+		nanoseconds = 0
+
+	case float64:
+		seconds = int64(timestamp)
+		nanoseconds = int64((timestamp - float64(seconds)) * 1e9)
+
+	default:
+		return utils.FailedResult[time.Time](fmt.Errorf("Unsupported timestamp type: %T", ev.Timestamp))
+	}
+
+	return utils.SuccessResult(time.Unix(seconds, nanoseconds).In(time.UTC).Truncate(time.Millisecond))
 }
 
 type FailedEvent struct {
@@ -25,4 +61,9 @@ type FailedEvent struct {
 	InitialErrorMessage string `json:"initial_error_message"`
 	ErrorMessage        string `json:"error_message"`
 	ErrorCode           string `json:"error_code"`
+}
+
+func roundToDecimalPlaces(num float64, places int) float64 {
+	shift := math.Pow10(places)
+	return math.Round(num*shift) / shift
 }
