1a9fcf6ad8f1447154dbbde879fb5ee7da635ae7
Vincent Pochet <vincent@getlago.com>
2025-03-20 15:51:44 +0100

    misc: Use connection pools (#492)
    * misc: Use connection pools
    
    * Ensure connection is closed

--- events_processor/config/database/database.go
+++ events_processor/config/database/database.go
@@ -1,8 +1,11 @@
 package database
 
 import (
+	"context"
 	"log/slog"
 
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
 	slogGorm "github.com/orandin/slog-gorm"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,15 +14,34 @@ import (
 type DB struct {
 	Connection *gorm.DB
 	logger     *slog.Logger
+	pool       *pgxpool.Pool
 }
 
 func NewConnection(dbUrl string) (*DB, error) {
 	logger := slog.Default()
 	logger = logger.With("component", "db")
 
-	dialector := postgres.Open(dbUrl)
+	poolConfig, err := pgxpool.ParseConfig(dbUrl)
+	if err != nil {
+		return nil, err
+	}
 
-	return OpenConnection(logger, dialector)
+	poolConfig.MaxConns = 200
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	dialector := postgres.New(postgres.Config{
+		Conn: stdlib.OpenDBFromPool(pool),
+	})
+
+	conn, err := OpenConnection(logger, dialector)
+	if err == nil {
+		conn.pool = pool
+	}
+	return conn, err
 }
 
 func OpenConnection(logger *slog.Logger, dialector gorm.Dialector) (*DB, error) {
@@ -36,4 +58,8 @@ func OpenConnection(logger *slog.Logger, dialector gorm.Dialector) (*DB, error) 
 	}
 
 	return &DB{Connection: db, logger: logger}, nil
+}
+
+func (db *DB) Close() {
+	db.pool.Close()
 }
