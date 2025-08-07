0db3e92690229828d7b5e84087aeadfd118820db
Vincent Pochet <vincent@getlago.com>
2025-08-07 15:20:23 +0200

    feat(pre-aggregation): Enrich events with Charge and Filters

--- events-processor/processors/event_processors/base_service.go
+++ events-processor/processors/event_processors/base_service.go
@@ -11,3 +11,14 @@ func failedResult(r utils.AnyResult, code string, message string) utils.Result[*
 	result.Capture = r.IsCapturable()
 	return result
 }
+
+func failedMultiEventsResult(r utils.AnyResult, code string, message string) utils.Result[[]*models.EnrichedEvent] {
+	result := utils.FailedResult[[]*models.EnrichedEvent](r.Error()).AddErrorDetails(code, message)
+	result.Retryable = r.IsRetryable()
+	result.Capture = r.IsCapturable()
+	return result
+}
+
+func toMultiEventsResult(r utils.Result[*models.EnrichedEvent]) utils.Result[[]*models.EnrichedEvent] {
+	return failedMultiEventsResult(r, r.ErrorCode(), r.ErrorMessage())
+}
