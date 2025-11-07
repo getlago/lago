b0209f617663476f983ebc5f4644abdd0a900c4c
Vincent Pochet <vincent@getlago.com>
2025-11-07 15:28:42 +0100

    misc(events-processor): Improve App setup (#626)

--- events-processor/processors/main_processor.go
+++ events-processor/processors/main_processor.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/twmb/franz-go/pkg/kgo"
 
-	tracer "github.com/getlago/lago/events-processor/config"
 	"github.com/getlago/lago/events-processor/config/database"
 	"github.com/getlago/lago/events-processor/config/kafka"
 	"github.com/getlago/lago/events-processor/config/redis"
@@ -18,8 +17,6 @@ import (
 )
 
 var (
-	ctx              context.Context
-	logger           *slog.Logger
 	processor        *events_processor.EventProcessor
 	apiStore         *models.ApiStore
 	kafkaConfig      kafka.ServerConfig
@@ -48,10 +45,12 @@ const (
 	envLagoRedisStorePassword                    = "LAGO_REDIS_STORE_PASSWORD"
 	envLagoRedisStoreURL                         = "LAGO_REDIS_STORE_URL"
 	envLagoRedisStoreTLS                         = "LAGO_REDIS_STORE_TLS"
-	envOtelExporterOtlpEndpoint                  = "OTEL_EXPORTER_OTLP_ENDPOINT"
-	envOtelInsecure                              = "OTEL_INSECURE"
-	envOtelServiceName                           = "OTEL_SERVICE_NAME"
 )
+
+type Config struct {
+	Logger       *slog.Logger
+	UseTelemetry bool
+}
 
 func initProducer(context context.Context, topicEnv string) utils.Result[*kafka.Producer] {
 	if os.Getenv(topicEnv) == "" {
@@ -77,7 +76,7 @@ func initProducer(context context.Context, topicEnv string) utils.Result[*kafka.
 	return utils.SuccessResult(producer)
 }
 
-func initFlagStore(name string) (*models.FlagStore, error) {
+func initFlagStore(ctx context.Context, name string) (*models.FlagStore, error) {
 	redisDb, err := utils.GetEnvAsInt(envLagoRedisStoreDB, 0)
 	if err != nil {
 		return nil, err
@@ -101,7 +100,7 @@ func initFlagStore(name string) (*models.FlagStore, error) {
 	return models.NewFlagStore(ctx, db, name), nil
 }
 
-func initChargeCacheStore() (*models.ChargeCache, error) {
+func initChargeCacheStore(ctx context.Context) (*models.ChargeCache, error) {
 	redisDb, err := utils.GetEnvAsInt(envLagoRedisCacheDB, 0)
 	if err != nil {
 		return nil, err
@@ -126,26 +125,10 @@ func initChargeCacheStore() (*models.ChargeCache, error) {
 	return chargeStore, nil
 }
 
-func StartProcessingEvents() {
-	ctx = context.Background()
-
-	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil)).
-		With("service", "post_process")
-	slog.SetDefault(logger)
-
-	otelEndpoint := os.Getenv(envOtelExporterOtlpEndpoint)
-	if otelEndpoint != "" {
-		telemetryCfg := tracer.TracerConfig{
-			ServiceName: os.Getenv(envOtelServiceName),
-			EndpointURL: otelEndpoint,
-			Insecure:    os.Getenv(envOtelInsecure),
-		}
-		tracer.InitOTLPTracer(telemetryCfg)
-	}
-
+func StartProcessingEvents(ctx context.Context, config *Config) {
 	serverBrokers := utils.ParseBrokersEnv(os.Getenv(envLagoKafkaBootstrapServers))
 	if len(serverBrokers) == 0 {
-		logger.Error("brokers not found")
+		config.Logger.Error("brokers not found")
 		panic("brokers not found")
 	}
 
@@ -153,42 +136,42 @@ func StartProcessingEvents() {
 		ScramAlgorithm: os.Getenv(envLagoKafkaScramAlgorithm),
 		TLS:            os.Getenv(envLagoKafkaTLS) == "true",
 		Servers:        serverBrokers,
-		UseTelemetry:   otelEndpoint != "",
+		UseTelemetry:   config.UseTelemetry,
 		UserName:       os.Getenv(envLagoKafkaUsername),
 		Password:       os.Getenv(envLagoKafkaPassword),
 	}
 
 	eventsEnrichedProducerResult := initProducer(ctx, envLagoKafkaEnrichedEventsTopic)
 	if eventsEnrichedProducerResult.Failure() {
-		logger.Error(eventsEnrichedProducerResult.ErrorMsg())
+		config.Logger.Error(eventsEnrichedProducerResult.ErrorMsg())
 		utils.CaptureErrorResult(eventsEnrichedProducerResult)
 		panic(eventsEnrichedProducerResult.ErrorMessage())
 	}
 
 	eventsEnrichedExpandedProducerResult := initProducer(ctx, envLagoKafkaEnrichedEventsExpandedTopic)
 	if eventsEnrichedExpandedProducerResult.Failure() {
-		logger.Error(eventsEnrichedExpandedProducerResult.ErrorMsg())
+		config.Logger.Error(eventsEnrichedExpandedProducerResult.ErrorMsg())
 		utils.CaptureErrorResult(eventsEnrichedExpandedProducerResult)
 		panic(eventsEnrichedExpandedProducerResult.ErrorMessage())
 	}
 
 	eventsInAdvanceProducerResult := initProducer(ctx, envLagoKafkaEventsChargedInAdvanceTopic)
 	if eventsInAdvanceProducerResult.Failure() {
-		logger.Error(eventsInAdvanceProducerResult.ErrorMsg())
+		config.Logger.Error(eventsInAdvanceProducerResult.ErrorMsg())
 		utils.CaptureErrorResult(eventsInAdvanceProducerResult)
 		panic(eventsInAdvanceProducerResult.ErrorMessage())
 	}
 
 	eventsDeadLetterQueueResult := initProducer(ctx, envLagoKafkaEventsDeadLetterTopic)
 	if eventsDeadLetterQueueResult.Failure() {
-		logger.Error(eventsDeadLetterQueueResult.ErrorMsg())
+		config.Logger.Error(eventsDeadLetterQueueResult.ErrorMsg())
 		utils.CaptureErrorResult(eventsDeadLetterQueueResult)
 		panic(eventsDeadLetterQueueResult.ErrorMessage())
 	}
 
 	maxConns, err := utils.GetEnvAsInt(envLagoEventsProcessorDatabaseMaxConnections, 200)
 	if err != nil {
-		logger.Error("Error converting max connections into integer", slog.String("error", err.Error()))
+		config.Logger.Error("Error converting max connections into integer", slog.String("error", err.Error()))
 		utils.CaptureError(err)
 		panic(err.Error())
 	}
@@ -200,24 +183,24 @@ func StartProcessingEvents() {
 
 	db, err := database.NewConnection(dbConfig)
 	if err != nil {
-		logger.Error("Error connecting to the database", slog.String("error", err.Error()))
+		config.Logger.Error("Error connecting to the database", slog.String("error", err.Error()))
 		utils.CaptureError(err)
 		panic(err.Error())
 	}
 	apiStore = models.NewApiStore(db)
 	defer db.Close()
 
-	flagger, err := initFlagStore("subscription_refreshed")
+	flagger, err := initFlagStore(ctx, "subscription_refreshed")
 	if err != nil {
-		logger.Error("Error connecting to the flag store", slog.String("error", err.Error()))
+		config.Logger.Error("Error connecting to the flag store", slog.String("error", err.Error()))
 		utils.CaptureError(err)
 		panic(err.Error())
 	}
 	defer flagger.Close()
 
-	cacher, err := initChargeCacheStore()
+	cacher, err := initChargeCacheStore(ctx)
 	if err != nil {
-		logger.Error("Error connecting to the charge cache store", slog.String("error", err.Error()))
+		config.Logger.Error("Error connecting to the charge cache store", slog.String("error", err.Error()))
 		utils.CaptureError(err)
 		panic(err.Error())
 	}
@@ -225,14 +208,14 @@ func StartProcessingEvents() {
 	defer chargeCacheStore.CacheStore.Close()
 
 	processor = events_processor.NewEventProcessor(
-		logger,
+		config.Logger,
 		events_processor.NewEventEnrichmentService(apiStore),
 		events_processor.NewEventProducerService(
 			eventsEnrichedProducerResult.Value(),
 			eventsEnrichedExpandedProducerResult.Value(),
 			eventsInAdvanceProducerResult.Value(),
 			eventsDeadLetterQueueResult.Value(),
-			logger,
+			config.Logger,
 		),
 		events_processor.NewSubscriptionRefreshService(flagger),
 		events_processor.NewCacheService(chargeCacheStore),
@@ -248,7 +231,7 @@ func StartProcessingEvents() {
 			},
 		})
 	if err != nil {
-		logger.Error("Error starting the event consumer", slog.String("error", err.Error()))
+		config.Logger.Error("Error starting the event consumer", slog.String("error", err.Error()))
 		utils.CaptureError(err)
 		panic(err.Error())
 	}
