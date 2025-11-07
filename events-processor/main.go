2bb6f9117f78840f4d1721b1e1fb4db3d497d893
Vincent Pochet <vincent@getlago.com>
2025-11-07 14:01:04 +0100

    misc(events-processor): Improve App setup

--- events-processor/main.go
+++ events-processor/main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
 	"os"
 	"time"
 
 	"github.com/getsentry/sentry-go"
 
+	tracer "github.com/getlago/lago/events-processor/config"
 	"github.com/getlago/lago/events-processor/processors"
 )
 
+const (
+	envEnv                      = "ENV"
+	envSentryDsn                = "SENTRY_DSN"
+	envOtelExporterOtlpEndpoint = "OTEL_EXPORTER_OTLP_ENDPOINT"
+	envOtelInsecure             = "OTEL_INSECURE"
+	envOtelServiceName          = "OTEL_SERVICE_NAME"
+)
+
 func main() {
+	ctx := context.Background()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil)).
+		With("service", "post_process")
+	slog.SetDefault(logger)
+
+	otelEndpoint := os.Getenv(envOtelExporterOtlpEndpoint)
+	if otelEndpoint != "" {
+		telemetryCfg := tracer.TracerConfig{
+			ServiceName: os.Getenv(envOtelServiceName),
+			EndpointURL: otelEndpoint,
+			Insecure:    os.Getenv(envOtelInsecure),
+		}
+		tracer.InitOTLPTracer(telemetryCfg)
+	}
+
 	err := sentry.Init(sentry.ClientOptions{
-		Dsn:              os.Getenv("SENTRY_DSN"),
-		Environment:      os.Getenv("ENV"),
+		Dsn:              os.Getenv(envSentryDsn),
+		Environment:      os.Getenv(envEnv),
 		Debug:            false,
 		AttachStacktrace: true,
 	})
@@ -25,5 +52,5 @@ func main() {
 	defer sentry.Flush(2 * time.Second)
 
 	// start processing events & loop forever
-	processors.StartProcessingEvents()
+	processors.StartProcessingEvents(ctx, &processors.Config{Logger: logger, UseTelemetry: otelEndpoint != ""})
 }
