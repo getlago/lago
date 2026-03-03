d6e127cb9c630feb6fa448e61ecf35a56454f88a
Vincent Pochet <vincent@getlago.com>
2026-03-03 15:23:42 +0100

    Add missing tests

---
+++ events-processor/utils/logger_test.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	"bytes"
+	"context"
+	"log/slog"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLevelHandler_Enabled(t *testing.T) {
+	inner := slog.NewJSONHandler(&bytes.Buffer{}, &slog.HandlerOptions{Level: slog.LevelDebug})
+	handler := NewLevelHandler(slog.LevelInfo, inner)
+
+	assert.False(t, handler.Enabled(context.Background(), slog.LevelDebug))
+	assert.True(t, handler.Enabled(context.Background(), slog.LevelInfo))
+	assert.True(t, handler.Enabled(context.Background(), slog.LevelWarn))
+	assert.True(t, handler.Enabled(context.Background(), slog.LevelError))
+}
+
+func TestLevelHandler_Handle(t *testing.T) {
+	var buf bytes.Buffer
+	inner := slog.NewJSONHandler(&buf, &slog.HandlerOptions{Level: slog.LevelDebug})
+	logger := slog.New(NewLevelHandler(slog.LevelInfo, inner))
+
+	logger.Debug("should be filtered")
+	assert.Empty(t, buf.String())
+
+	logger.Info("should pass")
+	assert.Contains(t, buf.String(), "should pass")
+}
+
+func TestLevelHandler_WithAttrs(t *testing.T) {
+	var buf bytes.Buffer
+	inner := slog.NewJSONHandler(&buf, &slog.HandlerOptions{Level: slog.LevelDebug})
+	logger := slog.New(NewLevelHandler(slog.LevelInfo, inner)).With("component", "kafka")
+
+	logger.Debug("should be filtered")
+	assert.Empty(t, buf.String())
+
+	logger.Info("should pass")
+	assert.Contains(t, buf.String(), "should pass")
+	assert.Contains(t, buf.String(), "kafka")
+}
+
+func TestLevelHandler_WithGroup(t *testing.T) {
+	var buf bytes.Buffer
+	inner := slog.NewJSONHandler(&buf, &slog.HandlerOptions{Level: slog.LevelDebug})
+	logger := slog.New(NewLevelHandler(slog.LevelInfo, inner)).WithGroup("kafka")
+
+	logger.Debug("should be filtered")
+	assert.Empty(t, buf.String())
+
+	logger.Info("should pass", "key", "value")
+	assert.Contains(t, buf.String(), "should pass")
+	assert.Contains(t, buf.String(), "kafka")
+}
